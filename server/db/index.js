bfb695dadb5720ad2fa14a1ae1199e5fc1abb3f6
Salma Noe <familypc@Salmas-Macbook-Air.local>
2021-10-01 10:22:27 -0700

    added more env variables for better deployment process

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,20 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 var dbConnection;
 dbConnection = mysql.createConnection({
-  user: 'root',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
-  database: 'sdcReviews'
+  database: 'sdcReviews',
+  charset: 'utf8mb4'
 });
-dbConnection.connect();
+dbConnection.connect((err) => {
+  if (err) {
+    console.log('errrrrrrrrr', err);
+  } else {
+    console.log('🦄', 'yessssssss');
+  }
+});
 
 module.exports = dbConnection;
