b2ede2cdedb1fb54082a2a32821cb52efeadcf59
Salma Noe <familypc@Salmas-Macbook-Air.local>
2021-10-01 18:52:23 -0700

    added loader io file

--- server/index.js
+++ server/index.js
@@ -12,6 +12,51 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome' });
 });
 
+app.get('/loaderio-21a884b39af0488d7834ab6caf215e36.txt', (req, res) => {
+    const options = {
+        root: path.join(__dirname)
+    };
+     
+    const fileName = 'loaderio-21a884b39af0488d7834ab6caf215e36.txt';
+    res.sendFile(fileName, options, (err) => {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+});
+
+app.get('/loaderio-21a884b39af0488d7834ab6caf215e36.html', (req, res) => {
+    const options = {
+        root: path.join(__dirname)
+    };
+     
+    const fileName = 'loaderio-21a884b39af0488d7834ab6caf215e36.html';
+    res.sendFile(fileName, options, (err) => {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+});
+
+app.get('/loaderio-21a884b39af0488d7834ab6caf215e36/', (req, res) => {
+    const options = {
+        root: path.join(__dirname)
+    };
+     
+    const fileName = 'loaderio-21a884b39af0488d7834ab6caf215e36/';
+    res.sendFile(fileName, options, (err) => {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+});
+
 require('./routes/index.js')(app);
 
 app.listen(port, () => {
